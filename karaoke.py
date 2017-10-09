7ea539a8493ede42f5be6dac7bcc46e1a8e2ef3f
Irene Vega Lago <ivega@gamma08.aulas.gsyc.urjc.es>
2017-10-09 17:26:02 +0200

    imprime por separado

--- karaoke.py
+++ karaoke.py
@@ -6,15 +6,18 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 
+if len(sys.argv) != 2:
+    sys.exit("  Usage: python3 karaoke.py file.smil")
 
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
     parser.setContentHandler(sHandler)
-
-    if len(sys.argv) != 2:
-        sys.exit("  Usage: python3 karaoke.py file.smil")
-
-    parser.parse(open('karaoke.smil'))
-    print(sHandler.list)
-
+    
+    parser.parse(open(sys.argv[1]))     
+    lista = sHandler.get_tags()
+#    print(lista)
+    for etiqueta in lista:
+        print(etiqueta['name'])
+        for atributo in etiqueta:
+            print(atributo)
